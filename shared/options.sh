41f7dae2b87df72b86c38e07d52480eaeee0c4eb
Michael Maloney <michael.maloney@apiture.com>
2024-03-10 16:02:09 -0400

    Update gitignore and add completion options, directory options, history options, input/output options, prompting options, completion menu, completion selection, completion messages, completion groupings, matching completions, completion colors, ambiguous completions, and auto-description options. Also, update package installation and initialization scripts.

--- shared/options.sh
+++ shared/options.sh
@@ -1,6 +1,9 @@
 autoload -Uz compinit
 compinit
 
+# |--------------------|
+# | Completion Options |
+# |--------------------|
 setopt AUTO_MENU
 setopt AUTO_LIST
 setopt AUTO_PARAM_SLASH
@@ -12,10 +15,18 @@ setopt HASH_LIST_ALL
 setopt LIST_TYPES
 setopt LIST_AMBIGUOUS
 setopt LIST_ROWS_FIRST
+
+# |----------------------------|
+# | Changing Directory Options |
+# |----------------------------|
 setopt AUTO_CD
 setopt AUTO_PUSHD
 setopt PUSHD_IGNORE_DUPS
 setopt CDABLE_VARS
+
+# |-----------------|
+# | History Options |
+# |-----------------|
 setopt INC_APPEND_HISTORY
 setopt HIST_FIND_NO_DUPS
 setopt HIST_IGNORE_ALL_DUPS
@@ -24,22 +35,60 @@ setopt HIST_NO_FUNCTIONS
 setopt HIST_REDUCE_BLANKS
 setopt HIST_VERIFY
 setopt SHARE_HISTORY
+
+# |----------------------|
+# | Input/Output Options |
+# |----------------------|
 setopt ALIASES
 setopt CLOBBER
-setopt CORRECT
 setopt HASH_DIRS
 setopt SHORT_LOOPS
+
+# |-------------------|
+# | Prompting Options |
+# |-------------------|
 setopt PROMPT_SUBST
 
+# |-----------------|
+# | Completion Menu |
+# |-----------------|
 zstyle ':completion:*' menu select completer
+
+# |----------------------|
+# | Completion Selection |
+# |----------------------|
 zstyle ':completion:*' select-prompt %SSelection at %p%s
+
+# |---------------------|
+# | Completion Messages |
+# |---------------------|
 zstyle ':completion:*:messages' format '%B%F{magenta}--- %d ---%f%b'
 zstyle ':completion:*:warnings' format '%B%F{red}--- no completions found ---%f%b'
 zstyle ':completion:*:*:*:*:descriptions' format '%B%F{magenta}--- %d ---%f%b'
 zstyle ':completion:*:*:*:*:corrections' format '%B%F{red}--- %d (errors: %e) ---%f%b'
+
+# |----------------------|
+# | Completion Groupings |
+# |----------------------|
 zstyle ':completion:*' group-name ''
+
+# |----------------------|
+# | Matching Completions |
+# |----------------------|
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*:matches' group 'yes'
+
+# |-------------------|
+# | Completion Colors |
+# |-------------------|
 zstyle ':completion:*:*' list-colors '=*=1;3;36'
+
+# |-----------------------|
+# | Ambiguous Completions |
+# |-----------------------|
 zstyle ':completion:*' insert-unambiguous true
+
+# |------------------|
+# | Auto-Description |
+# |------------------|
 zstyle ':completion:*' auto-description 'specify: %d'
--- shared/packages.sh
+++ shared/packages.sh
@@ -1,11 +1,51 @@
 function install () {
+    [[ "$(xcode-select -p)" != "/Library/Developer/CommandLineTools" ]] && xcode-select --install
+
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    [[ "$(uname -m)" == "x86_64" ]] && eval "$(/usr/local/bin/brew shellenv)"
+    [[ "$(uname -m)" == "arm64" ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    brew install rbenv ruby-build
+    rbenv install 3.2.2
+    rbenv global 3.2.2
+
+    brew install pyenv
+    pyenv install 3.12.0
+    pyenv global 3.12.0
+
+    brew install nvm
+
+    brew install go mage
+
+    brew install curl
+
+    brew install wget
+
+    brew install jq
+
+    brew install ack
+
+    brew install tree
+
+    brew install tldr
+
+    brew install htop
+
+    brew install bat
+
+    brew install zsh-autosuggestions
+
+    brew install zsh-syntax-highlighting
+
+    init
 }
 
 function init () {
+    [[ "$(uname -m)" == "x86_64" ]] && eval "$(/usr/local/bin/brew shellenv)"
+    [[ "$(uname -m)" == "arm64" ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
     eval "$(pyenv init -)"
     eval "$(rbenv init - zsh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
 
     export NVM_DIR="$HOME/.nvm"
     export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR="$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/highlighters"
@@ -18,4 +58,4 @@ function init () {
     [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
 }
 
-[[ -z $(which brew) ]] && install || init
+[[ "$(which brew)" == "brew not found" ]] && install || init
